f4e8cde2c58d6ddbdacdf4dcd62bf22ccd049318
jackonedev <jackone.action.software@gmail.com>
2024-09-23 20:57:00 +0000

    added module: frontend

---
+++ frontend/css.py
@@ -0,0 +1,11 @@
+css = """
+    @font-face {
+        font-family: IntelOne;
+        src: url("/file=./assets/intelone-bodytext-font-family-regular.ttf");
+    }
+    .gradio-container {background-color: #0a0c2b}
+    table {
+      border-collapse: collapse;
+      border: none;
+    }
+"""
---
+++ frontend/html.py
@@ -0,0 +1,33 @@
+# <td style="border-bottom:0"><img src="file/assets/DCAI_logo.png" height="300" width="300"></td>
+
+# html_title = '''
+# <table style="bordercolor=#0a0c2b; border=0">
+# <tr style="height:150px; border:0">
+#     <td style="border:0"><img src="/file=../assets/intel-labs.png" height="100" width="100"></td>
+#     <td style="vertical-align:bottom; border:0">
+#     <p style="font-size:xx-large;font-family:IntelOne, Georgia, sans-serif;color: white;">
+#      Multimodal RAG:
+#      <br>
+#      Chat with Videos
+#     </p>
+#     </td>
+#     <td style="border:0"><img src="/file=../assets/gaudi.png" width="100" height="100"></td>
+
+#     <td style="border:0"><img src="/file=../assets/IDC7.png" width="300" height="350"></td>
+#     <td style="border:0"><img src="/file=../assets/prediction_guard3.png" \
+#                                                               width="120" height="120"></td>
+# </tr>
+# </table>
+
+# '''
+
+html_title = """
+<table style="bordercolor=#0a0c2b; border=0">
+<tr style="height:150px; border:0">
+    <td style="border:0"><img src="/file=./assets/header.png"></td>
+</tr>
+</table>
+
+"""
+
+# <td style="border:0"><img src="/file=../assets/xeon.png" width="100" height="100"></td>
